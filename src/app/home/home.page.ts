f9381d9368fd524856d6738a267a59d50bcef9cf
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-09-20 20:52:51 +0100

    final before build

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  heigh: number;
+  weight: number;
+  bmi: number;
+  description: string;
+  imgSrc: string;
+
   constructor() {}
+
+  selectDescriptionAndImg(){
+    if(this.bmi < 18.5){
+      this.description = 'Underweight';
+      this.imgSrc= '../../assets/bmi/underweight.png';
+    }else if(this.bmi < 25){
+      this.description = 'Normal';
+      this.imgSrc= '../../assets/bmi/normal.png';
+    }else if(this.bmi < 30){
+      this.description = 'Overweight';
+      this.imgSrc= '../../assets/bmi/overweight.png';
+    }else if(this.bmi < 35){
+      this.description = 'Obese';
+      this.imgSrc= '../../assets/bmi/obese.png';
+    }else{
+      this.description = 'Extermely Obese';
+      this.imgSrc= '../../assets/bmi/ext_obese.png';
+    }
+
+  }
+
+  bmiCalculation(){
+    this.bmi = this.weight /(this.heigh*this.heigh)*10000;
+    this.selectDescriptionAndImg();
+  }
 
 }
